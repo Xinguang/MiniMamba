c6610a7d8b5c0fbf778bf0589b4b60ae65ee87db
Wang, Xinguang | LPD <ts-xinguang.wang@rakuten.com>
2025-07-01 14:21:41 +0900

    0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="minimamba",
-    version="0.1.0",
+    version="0.2.0",
     description="Minimal PyTorch implementation of Mamba (Selective State Space Model)",
     author="Xinguang",
     author_email="minimanba.github@kansea.com",
